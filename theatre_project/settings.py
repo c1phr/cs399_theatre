36d963ced6738096b900b08fa27284bb7febe24c
Eduardo Iniguez <ei36@nu.edu>
2015-02-02 23:51:49 -0700

    Added Bootstrap / Templates
    included updated url, views, and html files
    very broad and some information missing

--- theatre_project/settings.py
+++ theatre_project/settings.py
@@ -30,12 +30,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+
+   'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'theatre',
+    'theatre_project',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,5 +83,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
+
+
+#Template location
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),"theatre_project", "templates"),
+                  )
+
--- theatre_project/urls.py
+++ theatre_project/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
 # from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'theatre_project.views.home', name='home'),
+    url(r'^$', 'theatre_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    # url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^information/', views.information_performance, name='information_performance'),
+    url(r'^merchandise/', views.merchandise, name='merchandise'),
+    url(r'^performances/', views.performances, name='performances'),
+    url(r'^ticket_sales/', views.ticket_sales, name='ticket_sales'),
+    url(r'^admin/', include(admin.site.urls)),
+)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
